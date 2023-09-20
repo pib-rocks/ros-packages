e77f1a51a53a40dcc1a2c851e9f0e6e5b15aa53d
Lisabeth Hasse <lisabeth.hasse@isento.de>
2023-09-20 12:13:37 +0200

    refactor(PR-312): Remove user-decision

--- packages-set-up.sh
+++ packages-set-up.sh
@@ -12,37 +12,18 @@ ROS_VOICE_ASSISTANT_NODE_LINK="https://github.com/pib-rocks/voice-assistant/arch
 ROS_VOICE_ASSISTANT_NODE_DIR="$ROS_WORKING_DIR/src/voice_assistant"
 ROS_VOICE_ASSISTANT_NODE_ZIP="voice_assistant.zip"
 #
-# Ask the user if they want to install ROS2 packages
-read -p "Do you want to install all pib-packages? [A]ll, [S]ome, [N]one : " install_all
-if [ "$install_all" == "S" ]; then
-  # Check for individual packages to install
-  read -p "Install datatypes? [y]es, [n]o : " install_datatypes
-  read -p "Install motors? [y]es, [n]o : " install_motors
-  read -p "Install camera? [y]es, [n]o : " install_camera
-  read -p "Install voice-assistant? [y]es, [n]o : " install_voice_assistant
-fi
-if [[ "$install_all" == "A" || ("$install_all" == "S" && "$install_datatypes" == "y") ]] ; then
-  echo "Installing datatypes..."
-fi
-if [[ "$install_all" == "A" || ("$install_all" == "S" && "$install_motors" == "y") ]] ; then
-  echo "Installing motors..."
-fi
-if [[ "$install_all" == "A" || ("$install_all" == "S" && "$install_camera" == "y") ]] ; then
-  echo "Installing camera..."
-fi
-if [[ "$install_all" == "A" || ("$install_all" == "S" && "$install_voice_assistant" == "y") ]] ; then
-  echo "Installing voice-assistant..."
-  #
-  # Setting up the voice-assistant packages
-  pip3.10 install openai google-cloud-speech google-cloud-texttospeech pyaudio
-  sudo apt-get install flac
-  #
-  curl $ROS_VOICE_ASSISTANT_NODE_LINK -L --output $ROS_WORKING_DIR/$ROS_VOICE_ASSISTANT_NODE_ZIP
-  sudo unzip $ROS_WORKING_DIR/$ROS_VOICE_ASSISTANT_NODE_ZIP -d $ROS_WORKING_DIR
-  sudo chmod -R 777 $ROS_WORKING_DIR/voice-assistant-PR-312
-  mkdir $ROS_VOICE_ASSISTANT_NODE_DIR
-  mv $ROS_WORKING_DIR/voice-assistant-PR-312/*  $ROS_VOICE_ASSISTANT_NODE_DIR
-  rm -r $ROS_WORKING_DIR/voice-assistant-PR-312
-  #
-fi
+echo "Installing voice-assistant..."
+#
+# Setting up the voice-assistant packages
+pip3.10 install openai google-cloud-speech google-cloud-texttospeech pyaudio
+sudo apt-get install flac
+#
+# Downloading files for voice-assistant
+curl $ROS_VOICE_ASSISTANT_NODE_LINK -L --output $ROS_WORKING_DIR/$ROS_VOICE_ASSISTANT_NODE_ZIP
+sudo unzip $ROS_WORKING_DIR/$ROS_VOICE_ASSISTANT_NODE_ZIP -d $ROS_WORKING_DIR
+sudo chmod -R 777 $ROS_WORKING_DIR/voice-assistant-PR-312
+mkdir $ROS_VOICE_ASSISTANT_NODE_DIR
+mv $ROS_WORKING_DIR/voice-assistant-PR-312/*  $ROS_VOICE_ASSISTANT_NODE_DIR
+rm -r $ROS_WORKING_DIR/voice-assistant-PR-312
+#
 echo "Done with installing packages."
