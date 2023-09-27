0c206c5c005d00ed079d529dd6be5bc673b7c170
Lisabeth Hasse <lisabeth.hasse@isento.de>
2023-09-27 15:06:14 +0200

    build(PR-312): Add installation of camera
    - camera node installation from git-repo
    - installing necessary python packages
    - creating correct structure for camera in src

--- packages-set-up.sh
+++ packages-set-up.sh
@@ -8,6 +8,11 @@ DEFAULT_USER="pib"
 USER_HOME="/home/$DEFAULT_USER"
 ROS_WORKING_DIR="$USER_HOME/ros_working_dir"
 #
+ROS_CAMERA_NODE_LINK="https://github.com/Lisa-isento/ros2_oak_d_lite/archive/refs/heads/PR-312.zip"
+ROS_CAMERA_NODE_DIR="$ROS_WORKING_DIR/src/oak_d_lite"
+ROS_CAMERA_NODE_ZIP="oak_d_lite.zip"
+ROS_CAMERA_BOOT_DIR="$ROS_CAMERA_NODE_DIR/boot_scripts"
+#
 ROS_MOTORS_NODE_LINK="https://github.com/pib-rocks/motors/archive/refs/heads/PR-312.zip"
 ROS_MOTORS_NODE_DIR="$ROS_WORKING_DIR/src/motors"
 ROS_MOTORS_NODE_ZIP="motors.zip"
@@ -17,6 +22,28 @@ ROS_VOICE_ASSISTANT_NODE_LINK="https://github.com/pib-rocks/voice-assistant/arch
 ROS_VOICE_ASSISTANT_NODE_DIR="$ROS_WORKING_DIR/src/voice_assistant"
 ROS_VOICE_ASSISTANT_NODE_ZIP="voice_assistant.zip"
 ROS_VOICE_ASSISTANT_BOOT_DIR="$ROS_VOICE_ASSISTANT_NODE_DIR/boot_scripts"
+#
+echo "Installing ros2_oak_d_lite..."
+# Setting up the camera, including AI capabilities
+# Depth-AI
+sudo curl -sSL https://docs.luxonis.com/install_dependencies.sh | sudo bash
+python3 -m pip install depthai
+#Git examples for Depth-AI
+git clone --recurse-submodules https://github.com/luxonis/depthai-python.git
+cd depthai-python/examples
+python3 install_requirements.py
+# Hand tracker
+git clone https://github.com/geaxgx/depthai_hand_tracker.git
+cd depthai_hand_tracker
+pip install -r requirements.txt
+#
+# Downloading files for camera
+curl $ROS_CAMERA_NODE_LINK -L --output $ROS_WORKING_DIR/$ROS_CAMERA_NODE_ZIP
+sudo unzip $ROS_WORKING_DIR/$ROS_CAMERA_NODE_ZIP -d $ROS_WORKING_DIR
+sudo chmod -R 777 $ROS_WORKING_DIR/ros2_oak_d_lite-PR-312
+mkdir $ROS_CAMERA_NODE_DIR
+mv $ROS_WORKING_DIR/ros2_oak_d_lite-PR-312/*  $ROS_CAMERA_NODE_DIR
+rm -r $ROS_WORKING_DIR/ros2_oak_d_lite-PR-312
 #
 echo "Installing motors..."
 # Setting up the motor packages
